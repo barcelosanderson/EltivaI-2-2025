634e4070440d78eb280cfd98bd9c865079c67577
Anderson <frrbarcelos@gmail.com>
2025-08-22 14:51:48 -0300

    criando um navbar php separado para facilitar a vida

--- Lista_1/exercicio3.php
+++ Lista_1/exercicio3.php
@@ -10,26 +10,7 @@
 
 <body style="background: linear-gradient(135deg, #ff6600, #ff9933, #ffd280)">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="#">Lista 1 - Anderson Barcelos</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Exercícios
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="exercicio1.php">Exercício 1</a></li>
-            <li><a class="dropdown-item" href="exercicio2.php">Exercício 2</a></li>
-            <li><a class="dropdown-item" href="exercicio3.php">Exercício 3</a></li>
-          </ul>
-    </div>
-  </div>
-</nav>
+<?php include "navbar.php"; ?>
 
 <div class="d-flex justify-content-center vh-100">
 <div class="p-5 rounded shadow">
