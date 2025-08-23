e9f9d01b3eb14f5c469b3cf256d674b75b93e0e8
Anderson <frrbarcelos@gmail.com>
2025-08-22 22:14:00 -0300

    Professora, agora estou satisfeito com o meu CSS!!

---
+++ Lista_1/exercicio6.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Convertendo com o Anderson</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+</head>
+
+<body style="background: linear-gradient(135deg, #ff6600, #ff9933, #ffd280)">
+
+<?php include "navbar.php"; ?>
+
+<div class="d-flex justify-content-center vh-100">
+<div class="p-5 rounded shadow">
+<h1>Exercício 6 - Conversão °C para °F</h1>
+<form method="post">
+<div class="mb-3">
+    <label for="valor1" class="form-label">Informe a temperatura em °C: </label>
+    <input type="number" id="valor1" name="valor1" class="form-control" required="">
+</div>
+<div class="d-flex justify-content-center align-center">
+    <button type="submit" class="btn btn-dark">Enviar</button>
+</div>
+</form>
+<div class="mt-5 d-flex justify-content-center align-center">
+    
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $valor1 = $_POST["valor1"];
+            $conversao = ($valor1 * 1.8) + 32;
+            echo "<h1>°F: $conversao </h1>";
+            //- * --> multiplicação | ** --> potenciação | / --> divisão | % --> divisão sem resto
+        }
+    ?>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
--- Lista_1/navbar.php
+++ Lista_1/navbar.php
@@ -1,12 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
+
+  <nav class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="#">Lista 1 - Anderson Barcelos</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2">
-        <li class="nav-item dropdown">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Exercícios
           </a>
@@ -14,8 +13,13 @@
             <li><a class="dropdown-item" href="exercicio1.php">Exercício 1</a></li>
             <li><a class="dropdown-item" href="exercicio2.php">Exercício 2</a></li>
             <li><a class="dropdown-item" href="exercicio3.php">Exercício 3</a></li>
-            <li><a class="dropdown-item" href="exercicio3.php">Exercício 4</a></li>
+            <li><a class="dropdown-item" href="exercicio4.php">Exercício 4</a></li>
+            <li><a class="dropdown-item" href="exercicio5.php">Exercício 5</a></li>
+            <li><a class="dropdown-item" href="exercicio6.php">Exercício 6</a></li>
+            <li><a class="dropdown-item" href="exercicio7.php">Exercício 7</a></li>
           </ul>
+</ul>
     </div>
   </div>
-</nav>
+  </nav>
+</div>
