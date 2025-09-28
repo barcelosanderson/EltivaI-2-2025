6e25cd8b5f20b4b2acb02ad83c4ba3be0f116382
Anderson <frrbarcelos@gmail.com>
2025-09-28 01:27:36 -0300

    pos estudos lista 4

---
+++ Lista_4/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark p-1">
+  <a class="navbar-brand ms-2" href="#">Lista 4 - Anderson Barcelos</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#conteudoNavbarSuportado">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav dropdown">
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Exercícios
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="exercicio1.php">Exercício 1</a></li>
+        <li><a class="dropdown-item" href="exercicio2.php">Exercício 2</a></li>
+        <li><a class="dropdown-item" href="exercicio3.php">Exercício 3</a></li>
+        <li><a class="dropdown-item" href="exercicio4.php">Exercício 4</a></li>
+        <li><a class="dropdown-item" href="exercicio5.php">Exercício 5</a></li>
+      </ul>
+    </ul>
+  </div>
+  </div>
+</nav>
+</div>
